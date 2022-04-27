75193ae97c9ee8621ebe9916a01028d7e3cf1757
Demonbek <dddlll@inbox.ru>
2022-04-27 22:35:45 +0300

    ggg

---
+++ app/src/main/java/ru/demonapps/youtube/utils/Constants.java
@@ -0,0 +1,11 @@
+package ru.demonapps.youtube.utils;
+
+/**
+ * Created by sonu on 10/11/17.
+ */
+
+public class Constants {
+
+    //youtube developer API Key
+    public static String DEVELOPER_KEY = "YOUR_DEVELOPER_API_KEY";
+}
